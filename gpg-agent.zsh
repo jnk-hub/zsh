63dbc346a6f1e7ff2755e8906dfc07c621416cd3
ps <ya.jnk@ya.ru>
2026-03-18 21:03:08 +0300

    add gpg-agent zsh

---
+++ gpg-agent.zsh
@@ -0,0 +1,6 @@
+# GPG-Agent
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+gpg-connect-agent updatestartuptty /bye &>/dev/null
